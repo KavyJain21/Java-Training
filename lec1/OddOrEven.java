8d65282bd63cced25c799a52155fc815dfadd4c9
Kavy Jain <kavyj942@gmail.com>
2025-11-14 15:40:52 +0530

    Basics

---
+++ lec1/OddOrEven.java
@@ -0,0 +1,13 @@
+package lec1;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int a =5;
+        if(a%2==0){
+            System.out.println("even");
+
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
---
+++ lec1/Palindrome.java
@@ -0,0 +1,16 @@
+package lec1;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a");
+        int a=sc.nextInt();
+           System.out.println("Enter value of b");
+           int b =sc.nextInt();
+           int sum=a+b;
+           System.out.println(sum);
+    }
+}
+
